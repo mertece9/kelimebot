5900d8be6b850455b467448d3c687e3944ff0125
mertece9 <112589239+mertece9@users.noreply.github.com>
2022-09-21 19:40:14 +0300

    Add files via upload

---
+++ kelime_bot/helpers/keyboards.py
@@ -0,0 +1,8 @@
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup
+
+kanal = InlineKeyboardMarkup([
+    [InlineKeyboardButton("ℹ️ Resmi Kanal " , url= "t.me/ProTubeSupport")]
+])
+
+
