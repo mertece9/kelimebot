4b296e3867c286e2a801e19182990a4ea68dc2fe
mertece9 <112589239+mertece9@users.noreply.github.com>
2022-09-21 19:38:10 +0300

    Delete kelime_bot directory

--- kelime_bot/__main__.py
+++
@@ -1,4 +0,0 @@
-from kelime_bot import bot
-
-if __name__ == "__main__":
-    bot.run()
--- kelime_bot/plugins/data.py
+++
@@ -1,8 +0,0 @@
-from kelime_bot import oyun, rating
-from pyrogram import Client, filters
-
-
-@Client.on_message(filters.command("data") & filters.user("GKKurucu")) 
-async def data(c:Client, m):
-    await m.reply(oyun)
-    await m.reply(rating)
